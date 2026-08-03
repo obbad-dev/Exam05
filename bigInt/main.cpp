45561265848d7aa5c4d29a9671143c0918ebef38
oulidobbad <oualidobbad@gmail.com>
2026-08-03 21:34:25 +0100

    f

--- bigInt/main.cpp
+++ bigInt/main.cpp
@@ -1,5 +1,7 @@
 #include "bigint.hpp"
-int main ()
+#include <iostream>
+
+int main()
 {
     const bigint a(42);
     bigint b(21), c, d(1337), e(d);
@@ -13,26 +15,22 @@ int main ()
     std::cout << "a + b = " << a + b << std::endl;
     std::cout << "(c += a) = " << (c += a) << std::endl;
 
-	std::cout << "b = " << b << std::endl;
-	std::cout << "++b = " << ++b << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "++b = " << ++b << std::endl;
     std::cout << "b++ = " << b++ << std::endl;
 
+    // d = 1;
     std::cout << "(b << 10) + 42 = " << ((b << 10) + 42) << std::endl;
-//    // std::cout << "(d >>= 4) = " << (d >>= 4) << std::endl;
     std::cout << "(d <<= 4) = " << (d <<= 4) << std::endl;
-    // std::cout << "(c <<= 4) = " << (c <<= 4) << std::endl;
-    // std::cout << "(c >= 4) = " << (c >>= 4) << std::endl;
+    std::cout << "(d >>= 2) = " << (d >>= (const bigint)2) << std::endl;
 
-   	std::cout << "(d >>= 2) = " << (d >>= (const bigint)2) << std::endl;
-    std::cout << "a =" << a << std::endl;
-    std::cout << "d =" << d << std::endl;
+    // std::cout << "a =" << a << std::endl;
+    // std::cout << "d =" << d << std::endl;
 
-    std::cout << "(d < a) = " << (d < a) << std::endl;
-    std::cout << "(d <= a) = " << (d <= a) << std::endl;
-    std::cout << "(d > a) = " << (d > a) << std::endl;
-    std::cout << "(d >= a) = " << (d >= a) << std::endl;
-    std::cout << "(d == a) = " << (d == a) << std::endl;
-    std::cout << "(d != a) = " << (d != a) << std::endl;
-
-	return 0;
+    // std::cout << "(d < a) = " << (d < a) << std::endl;
+    // std::cout << "(d <= a) = " << (d <= a) << std::endl;
+    // std::cout << "(d > a) = " << (d > a) << std::endl;
+    // std::cout << "(d >= a) = " << (d >= a) << std::endl;
+    // std::cout << "(d == a) = " << (d == a) << std::endl;
+    // std::cout << "(d != a) = " << (d != a) << std::endl;
 }
